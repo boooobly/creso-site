6e24909de8cd1327db58be17d2d5f95bd3727e0d
boooobly <avatar99266@mail.ru>
2025-09-18 17:03:19 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,14 @@
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      { protocol: "https", hostname: "images.ctfassets.net" },
+      { protocol: "https", hostname: "assets.ctfassets.net" },
+    ],
   },
   redirects: async () => [
     { source: "/", destination: "/ru", permanent: false }
-  ]
+  ],
 };
+
 export default nextConfig;
